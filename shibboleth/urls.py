7111b478183af6ceb61d6d633ae346d5b6429962
Brad Busenius <bbusenius@users.noreply.github.com>
2016-01-15 15:50:22 -0600

    Removed '' in urls.py.

--- shibboleth/urls.py
+++ shibboleth/urls.py
@@ -11,7 +11,7 @@ if (sys.version_info < (3, 0)):
 else:
     from .views import ShibbolethView, ShibbolethLogoutView, ShibbolethLoginView
 
-urlpatterns = ['',
+urlpatterns = [
     url(r'^login/$', ShibbolethLoginView.as_view(), name='login'),
     url(r'^logout/$', ShibbolethLogoutView.as_view(), name='logout'),
     url(r'^$', ShibbolethView.as_view(), name='info'),
